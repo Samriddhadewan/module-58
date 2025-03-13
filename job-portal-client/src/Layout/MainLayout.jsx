214c8c23c9d5f26720fe142953eb3c74432c7f64
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-03-13 22:34:20 +0600

    video 7 done

--- job-portal-client/src/Layout/MainLayout.jsx
+++ job-portal-client/src/Layout/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 const MainLayout = () => {
   return (
-    <div className='max-w-7xl mx-auto'>
+    <div>
+      <div className='max-w-7xl mx-auto'>
       <Navbar></Navbar>
-      <Outlet></Outlet>
+      <Outlet></Outlet>     
+    </div>
+    <Footer></Footer>
     </div>
   )
 }
