b2fc63c2893f433de4238eb37155dd074e75eae1
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-03-17 14:08:21 +0600

    backed of job count functionality learned

--- jobsPortal-serverSide/index.js
+++ jobsPortal-serverSide/index.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-
-// middlewares 
+// middlewares
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mx4ls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,7 +26,92 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const jobCollection = client.db("job-portal").collection("jobs");
+    const jobApplyCollection = client
+      .db("job-portal")
+      .collection("applications");
+
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(cursor);
+      res.send(result);
+    });
+
+    app.post("/jobApplication", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplyCollection.insertOne(application);
+
+
+      
+      const id = application.job_Id;
+      const query = { _id: new ObjectId(id) };
+      const job = await jobCollection.findOne(query);
+      let count = 0;
+      if (job.applicationCount) {
+        count = job.applicationCount + 1;
+      } else {
+        count = 1;
+      }
+
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          applicationCount: count
+        },
+      };
+
+      const updateResult = await jobCollection.updateOne(
+        filter,
+        updateDoc
+      );
+      res.send(result);
+    });
+
+    app.get("/jobApplications", async (req, res) => {
+      const email = req.query.email;
+      const query = { user_email: email };
+      const result = await jobApplyCollection.find(query).toArray();
+
+      for (const application of result) {
+        const query = { _id: new ObjectId(application.job_Id) };
+        const res = await jobCollection.findOne(query);
+        if (res) {
+          application.title = res.title;
+          application.company = res.company;
+          application.company_logo = res.company_logo;
+        }
+      }
+      res.send(result);
+    });
+
+    app.post("/jobs", async (req, res) => {
+      const application = req.body;
+      const result = await jobCollection.insertOne(application);
+
+      res.send(result);
+    });
+
+    app.get("/postedJobs", async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email };
+      }
+      const cursor = jobCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,14 +119,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("job is falling from the sky");
+});
 
-
-
-
-app.get("/",(req, res)=>{
-    res.send("job is falling from the sky")
-})
-
-app.listen(port, ()=>{
-    console.log(`Job is waiting at:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Job is waiting at:${port}`);
+});
