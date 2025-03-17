6ff82212f5e3b45a283577a6e2cbfc010d537823
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-03-17 14:09:56 +0600

    job application count functionality learned

--- job-portal-client/src/Pages/MyPostedJobs.jsx
+++ job-portal-client/src/Pages/MyPostedJobs.jsx
@@ -1,20 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import useAuth from '../hooks/useAuth';
+import React, { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 const MyPostedJobs = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
   const [jobs, setJobs] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`http://localhost:5000/postedJobs?email=${user.email}`)
-    .then(res=> res.json())
-    .then(data => setJobs(data))
-  } ,[user.email])
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, [user.email]);
+  console.log(jobs)
   return (
     <div>
       <h1>My Posted Jobs {jobs.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Deadline</th>
+              <th>Company Name</th>
+              <th>Job Applied</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobs.map((item, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>{item.title}</td>
+                <td>{item.deadline}</td>
+                <td>{item.company}</td>
+                <td>{item.applicationCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyPostedJobs
+export default MyPostedJobs;
